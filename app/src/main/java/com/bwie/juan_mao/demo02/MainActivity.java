8f8e450fed5158a24c986154cb06d170aeef516a
猫绪草莓酱 <1327881137@qq.com>
2018-11-27 14:44:19 +0800

    this is second commit

--- app/src/main/java/com/bwie/juan_mao/demo02/MainActivity.java
+++ app/src/main/java/com/bwie/juan_mao/demo02/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 这是首页
+
+        // 这是轮播图
+
+        // 这是秒杀倒计时
+
+        // 这是商品推荐
     }
 }
